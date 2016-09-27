2ee66bfb2d8a28498eb1d44c6053cf321bd09590
Shashikumar D <scorpion.sk5@hotmail.com>
2016-09-27 19:18:26 +0530

    Added new repository. HashHandler

---
+++ HashHandler/src/HashHandler.js
@@ -0,0 +1,37 @@
+﻿(function (window) {
+    var registeredListeners = [],
+
+        findIndex = function (handler) {
+            return registeredListeners.indexOf(handler);
+        },
+
+        triggerHandlers = function (e) {
+            registeredListeners.forEach(function (handler) {
+                (typeof handler === 'function') && handler(e);
+            });
+        },
+
+        HashHandler = Object.create({
+            start: function () {
+                if (typeof window.onhashchange === 'object') {
+                    window.onhashchange = triggerHandlers;
+                }
+                else {
+
+                }
+            },
+
+            addHandler: function (handler) {
+                if (findIndex(handler) == -1) {
+                    registeredListeners.push(handler);
+                }
+            },
+
+            removeHandler: function (handler) {
+                var index = findIndex(handler);
+                if (index > -1) {
+                    registeredListeners.splice(index, 1);
+                }
+            }
+        });
+})(window);
