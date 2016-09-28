59d87baf6fe418a9a212390742796aaade9c3bc5
Shashikumar D <scorpion.sk5@hotmail.com>
2016-09-28 19:40:30 +0530

    update

--- HashHandler/src/HashHandler.js
+++ HashHandler/src/HashHandler.js
@@ -1,24 +1,66 @@
-﻿(function (window) {
+﻿(function (window, define) {
     var registeredListeners = [],
+
+        prevHash = '',
+
+        extend = function () {
+            var newObject = {};
+            for (var idx in arguments) {
+                for (var objName in arguments[idx]) {
+                    newObject[objName] = arguments[idx][objName];
+                };
+            };
+
+            return newObject;
+        },
+
+        getDefaultConfig = function () {
+            return {
+                canRepeat: false
+            };
+        },
 
         findIndex = function (handler) {
             return registeredListeners.indexOf(handler);
         },
 
         triggerHandlers = function (e) {
-            registeredListeners.forEach(function (handler) {
-                (typeof handler === 'function') && handler(e);
-            });
+            var me = this,
+                args = {
+                    e: e,
+                    prevHash: prevHash,
+                    newHash: me.getHash()
+                };
+
+            if (me.config.canRepeat || args.newHash !== args.prevHash) {
+                registeredListeners.forEach(function (handler) {
+                    (typeof handler === 'function') && handler(args);
+                });
+            };
+
+            prevHash = me.getHash();
+        },
+
+        trimHashString = function (hashString) {
+            if (hashString[0] === '#')
+                hashString = hashString.substring(1, hashString.length);
+            return hashString;
         },
 
         hashHandler = Object.create({
-            start: function () {
+            start: function (config) {
+                var hashString = window.document.location.hash;
+
+                this.config = extend(getDefaultConfig(), config || {});
+
                 if (typeof window.onhashchange === 'object') {
-                    window.onhashchange = triggerHandlers;
+                    window.onhashchange = triggerHandlers.bind(this);
                 }
                 else {
                     // TODO: Non-supported browser implementation.
-                }
+                };
+
+                prevHash = hashString ? trimHashString(hashString) : '';
             },
 
             addHandler: function (handler) {
@@ -34,8 +76,18 @@
                 }
             },
 
-            reset: function () {
+            reset: function (newConfig) {
                 registeredListeners = [];
+                this.config = extend(getDefaultConfig(), newConfig || {});
+            },
+
+            getHash: function () {
+                var hashString = window.document.location.hash;
+                return hashString ? trimHashString(hashString) : '';
+            },
+
+            setHash: function (hash) {
+                window.document.location.hash = hash;
             }
         });
 
@@ -46,4 +98,5 @@
     else {
         window.hashHandler = hashHandler;
     };
-})(window);
+
+})(window, define);
