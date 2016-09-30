9ffa6bd8efc22ac26598763bc051600a72d73895
scorpionsk5 <sashikumar148@gmail.com>
2016-09-30 22:16:00 +0530

    update

--- tools/build.js
+++ tools/build.js
@@ -6,6 +6,8 @@
         if (error)
             console.error(error.message);
         else {
+            var licenseText = '/*Copyright (c) 2016 Shashi Kumar D, for more details visit - */';
+            data = licenseText + data;
             fs.writeFile('build/hashHandler.min.js', data, function (err, fileRef) {
                 if (err) {
                     console.log(err);
