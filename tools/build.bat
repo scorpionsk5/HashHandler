7a5e968749ff96c7cf13c8c5a67d7f04de1a847c
Shashikumar D <scorpion.sk5@hotmail.com>
2016-09-30 15:04:47 +0530

    update

---
+++ tools/build.bat
@@ -0,0 +1,6 @@
+﻿echo off;
+npm init
+cd NodeJs;
+node.exe $ npm install glup;
+node.exe %~dp0build.js;
+pause
---
+++ tools/build.js
@@ -0,0 +1,11 @@
+﻿(function (require) {
+    console.log('test');
+    var gulp = require('gulp'),
+        minify = require('gulp-uglify'),
+        concat = require('gulp-concat');
+    
+    //gulp.task('Build', function () {
+        
+    //});
+
+})(require);
